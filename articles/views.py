2bf211d2f7e8f0f30c849eb9bd8d4708f588f1e7
alighare <gharekhaniali78@gmail.com>
2020-07-06 23:56:25 +0430

    day 3

--- articles/views.py
+++ articles/views.py
@@ -1,8 +1,13 @@
 
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 from . import models
 
 def article_list(request):
     articles=models.Articles.objects.all()
     args={"articles":articles}
     return render(request,"articles/article_list.html",args)
+def articles_details(request,slug):
+    # return HttpResponse(slug)
+    articles=models.Articles.objects.get(slug=slug)
+    args={"articles":articles}
+    return render(request,"articles/article_details.html",args)
--- django_blog/settings.py
+++ django_blog/settings.py
@@ -122,3 +122,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS=(
 os.path.join(BASE_DIR,"static_files"),
 )
+MEDIA_URL='/mwdial/'
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
